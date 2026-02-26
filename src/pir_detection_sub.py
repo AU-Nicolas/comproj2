07bcda8f0988d35981c51cabc7079d62f37d8998
Lighty <lightymail@mail.dk>
2026-02-26 12:14:46 +0100

    it1

--- src/pir_detection_sub.py
+++ src/pir_detection_sub.py
@@ -2,41 +2,54 @@ from time import sleep
 import paho.mqtt.client as mqtt
 import json
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+# Når lokale klient forbinder til localhost
+def on_connect_local(client, userdata, flags, rc):
+    print("Connected local with result code "+str(rc))
+    # Lokale klient subscriber til sensor
     client.subscribe("zigbee2mqtt/zone_1")
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    
+# Når offentlige klient forbinder til IP
+def on_connect_public(client, userdata, flags, rc):
+    print("Connected public with result code "+str(rc))
+
+# Når lokale klient modtager besked fra sensor
+def on_sensor_message(client, userdata, msg):
+    # Konverterer besked fra json til dictionary
     payload_str = msg.payload.decode("utf-8")
-    
     data = json.loads(payload_str)
     
+    # Data extractes fra dictionary
     illuminance = data.get("illuminance")
     linkquality = data.get("linkquality")
     occupancy = data.get("occupancy")
     
+    # Public klient sender besked over IP baseret på data fra sensor
     if (occupancy):
-        client.publish("detection", "there is a human")
+        client_public.publish("detection", "there is a human")
     else:
-        client.publish("detection", "there is not a human")
+        client_public.publish("detection", "there is not a human")
     
     print("god message")
     
     
-def on_publish(client, userdata, message_id):
-    print(f"message with ID {message_id} published")
+# Lokale klient laves
+client_local = mqtt.Client()
+# Connect-funktion sættes
+client_local.on_connect = on_connect_local
+# On_message funktion sættes
+client_local.on_message = on_sensor_message
 
-client = mqtt.Client()
-# Client callback that is called when the client successfully connects to the broker.
-client.on_connect = on_connect
-# Client callback that is called when a message within the subscribed topics is published.
-client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a manual interface.
-client.loop_forever()
+# Public client laves
+client_public = mqtt.Client()
+# Connect-funktion sættes
+client_public.on_connect = on_connect_public
+
+# Klienter forbinder til IP-adresser
+client_local.connect("localhost", 1883, 60)
+client_public.connect("10.112.196.211", 1883, 60)
+
+
+# Lokale klient lytter konstant
+client_local.loop_forever()
